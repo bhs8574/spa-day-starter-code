e04958300fed9442a634e576c469819169882827
bhs8574 <bhs8574@gmail.com>
2020-06-07 14:34:40 -0500

    Completed first bonus mission.

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,16 +1,20 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.data.UserData;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @GetMapping
+    public String displayAllUsers(Model model) {
+        model.addAttribute("users", UserData.getAll());
+        return "user/index";
+    }
 
     @RequestMapping("add")
     public String displayAddUserForm() {
@@ -18,16 +22,27 @@ public class UserController {
     }
 
     @PostMapping
-    @GetMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
+    public String processAddUserForm(@ModelAttribute User newUser, Model model, String verify) {
+        if (newUser.getPassword().equals(verify)) {
+            UserData.add(newUser);
+            return "redirect:user";
         } else {
             model.addAttribute("error", true);
-            model.addAttribute("username", user.getUsername());
-            model.addAttribute("email", user.getEmail());
+            model.addAttribute("username", newUser.getUsername());
+            model.addAttribute("email", newUser.getEmail());
             return "user/add";
         }
+    }
+
+    @GetMapping("details/{id}")
+    public String displayUserInfo(Model model, @PathVariable int id) {
+        //int userId = Integer.parseInt(id);
+        if (UserData.getById(id) != null & id > 0) {
+            model.addAttribute("user", UserData.getById(id));
+        } else {
+            model.addAttribute("invalidUserId", true);
+        }
+        return "user/details";
     }
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,10 +1,22 @@
 package org.launchcode.spaday.models;
 
+import java.util.Objects;
+
 public class User {
 
+    private int id;
+    private static int nextId = 1;
     private String username;
     private String email;
     private String password;
+
+    public User(String username, String email, String password) {
+        this.id = nextId;
+        nextId++;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
@@ -28,5 +40,32 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
